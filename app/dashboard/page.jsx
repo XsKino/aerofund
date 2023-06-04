fa73c37c44c8d4c95d4d95a1bcfd437a1ae8600f
Sergio Ivan Pinon Pena <sergioivan.pinon@gmail.com>
2023-06-04 15:03:03 -0600

    nueva venta
    ya ultimo commit

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,17 +1,24 @@
-import Image from "next/image"
-import Link from "next/link"
-import NavBar from "@/components/NavBar"
+import React from 'react';
+import NavBar from '@/components/NavBar';
+import Link from 'next/link';
 
 export default function Dashboard() {
   return (
-    <main className='w-screen h-screen bg-red-500'>
+    <main className="w-screen h-screen bg-red-500">
       <NavBar />
-      <div id='body' className='w-full'>
-        <br />
-        <Link href='/fund'>soy un proyecto, clickeame</Link>
-        <br />
-        <Link href='/upload'>Upload</Link>
+      <div id="body" className="w-full flex flex-col items-center justify-center">
+        <p className="text-3xl text-white mb-8 font-bold">
+          Aqui puedes ver proyectos y puedes subir tu proyecto, que esperas
+        </p>
+        <div className="flex items-center justify-center space-x-4">
+          <Link className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded" href="/fund">
+              Soy un proyecto, clickeame
+          </Link>
+          <Link className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded" href="/upload">
+              Upload
+          </Link>
+        </div>
       </div>
     </main>
-  )
+  );
 }
