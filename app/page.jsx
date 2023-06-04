ef3aabd5062acc4e2445f52a1c4347cc4b725277
Andres P <andresprza@gmail.com>
2023-06-04 11:28:15 -0600

    restore

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,35 @@
 "use client"
 import Image from "next/image"
 import Link from "next/link"
-import { getPublicKey, getBalance } from "@/services/wallet"
 import { useState } from "react"
-import toast, { Toaster } from "react-hot-toast"
 import NavBar from "@/components/NavBar"
+import { getPublicKey, getBalance } from "@/services/wallet"
 
-export default function Home({ connectWallet, publicKey, balance }) {
+export default function Home() {
+  const [publicKey, setPublicKey] = useState(null)
+  const [balance, setBalance] = useState(null)
+
+  const connectWallet = async () => {
+    let publicKeyTemp = await getPublicKey()
+    if (typeof publicKeyTemp !== "string") {
+      console.log(publicKeyTemp)
+      toast.error("Phantom Wallet not installed")
+      setTimeout(() => {
+        open("https://phantom.app/download", "_blank")
+      }, 1600)
+    }
+    let balanceTemp
+    if (!publicKeyTemp) return
+    if (!balance) {
+      balanceTemp = await getBalance(publicKeyTemp)
+    }
+    setPublicKey(publicKeyTemp)
+    setBalance(balanceTemp)
+
+    localStorage.setItem("publicKey", publicKey)
+    location.replace("/dashboard")
+  }
+
   return (
     <main className='w-full'>
       <NavBar connectWallet={connectWallet} />
@@ -69,150 +92,110 @@ export default function Home({ connectWallet, publicKey, balance }) {
           </div>
         </div>
       </section>
-      <section className='bg-white'>
-        <div className='py-8 px-4 flex flex-wrap mx-auto max-w-screen-xl text-left lg:py-16 lg:px-6'>
-          <div className='max-w-screen-sm'>
-            <h2 className='mb-4 text-4xl tracking-tight font-extrabold text-gray-900'>
-              Testimonials
-            </h2>
-            <p className='mb-8 font-light text-gray-500 lg:mb-16 sm:text-xl'>
+      <section class='bg-white'>
+        <div class='py-8 px-4 flex flex-wrap mx-auto max-w-screen-xl text-left lg:py-16 lg:px-6'>
+          <div class='max-w-screen-sm'>
+            <h2 class='mb-4 text-4xl tracking-tight font-extrabold text-gray-900'>Testimonials</h2>
+            <p class='mb-8 font-light text-gray-500 lg:mb-16 sm:text-xl'>
               Explore the whole collection of open-source web components and elements built with the
-              utility classNamees from Tailwind
+              utility classes from Tailwind
             </p>
           </div>
-          <div className='grid mb-8 lg:mb-12 lg:grid-cols-2'>
-            <figure className='flex flex-col justify-center items-center p-8 text-center bg-gray-50 border-b border-gray-200 md:p-12 lg:border-r'>
-              <blockquote className='mx-auto mb-8 max-w-2xl text-gray-500'>
-                <h3 className='text-lg font-semibold text-gray-900'>
+          <div class='grid mb-8 lg:mb-12 lg:grid-cols-2'>
+            <figure class='flex flex-col justify-center items-center p-8 text-center bg-gray-50 border-b border-gray-200 md:p-12 lg:border-r'>
+              <blockquote class='mx-auto mb-8 max-w-2xl text-gray-500'>
+                <h3 class='text-lg font-semibold text-gray-900'>
                   Speechless with how easy this was to integrate
                 </h3>
-                <p className='my-4'>
+                <p class='my-4'>
                   "I recently got my hands on Flowbite Pro, and holy crap, I'm speechless with how
                   easy this was to integrate within my application. Most templates are a pain, code
                   is scattered, and near impossible to theme.
                 </p>
-                <p className='my-4'>
+                <p class='my-4'>
                   Flowbite has code in one place and I'm not joking when I say it took me a matter
                   of minutes to copy the code, customise it and integrate within a Laravel + Vue
                   application.
                 </p>
-                <p className='my-4'>If you care for your time, I hands down would go with this."</p>
+                <p class='my-4'>If you care for your time, I hands down would go with this."</p>
               </blockquote>
               <img
-                className='w-9 h-9 rounded-full'
+                class='w-9 h-9 rounded-full'
                 src='https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png'
                 alt='profile picture'
               />
-              <div className='space-y-0.5 font-medium text-left'>
+              <div class='space-y-0.5 font-medium text-left'>
                 <div>Bonnie Green</div>
-                <div className='text-sm font-light text-gray-500'>Developer at Open AI</div>
+                <div class='text-sm font-light text-gray-500'>Developer at Open AI</div>
               </div>
             </figure>
-            <figure className='flex flex-col justify-center items-center p-8 text-center bg-gray-50 border-b border-gray-200 md:p-12'>
-              <blockquote className='mx-auto mb-8 max-w-2xl'>
-                <h3 className='text-lg font-semibold text-gray-900 '>
+            <figure class='flex flex-col justify-center items-center p-8 text-center bg-gray-50 border-b border-gray-200 md:p-12'>
+              <blockquote class='mx-auto mb-8 max-w-2xl'>
+                <h3 class='text-lg font-semibold text-gray-900 '>
                   Solid foundation for any project
                 </h3>
-                <p className='my-4'>
+                <p class='my-4'>
                   "FlowBite provides a robust set of design tokens and components based on the
                   popular Tailwind CSS framework. From the most used UI components like forms and
                   navigation bars to the whole app screens designed both for desktop and mobile,
                   this UI kit provides a solid foundation for any project.
                 </p>
-                <p className='my-4'>
+                <p class='my-4'>
                   Designing with Figma components that can be easily translated to the utility
-                  classNamees of Tailwind CSS is a huge timesaver!"
+                  classes of Tailwind CSS is a huge timesaver!"
                 </p>
               </blockquote>
               <img
-                className='w-9 h-9 rounded-full'
+                class='w-9 h-9 rounded-full'
                 src='https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/roberta-casas.png'
                 alt='profile picture'
               />
-              <div className='space-y-0.5 font-medium text-left'>
+              <div class='space-y-0.5 font-medium text-left'>
                 <div>Roberta Casas</div>
-                <div className='text-sm font-light text-gray-500 '>Lead designer at Dropbox</div>
+                <div class='text-sm font-light text-gray-500 '>Lead designer at Dropbox</div>
               </div>
             </figure>
-            <figure className='flex flex-col justify-center items-center p-8 text-center bg-gray-50 border-b border-gray-200 lg:border-b-0 md:p-12 lg:border-r'>
-              <blockquote className='mx-auto mb-8 max-w-2xl text-gray-500'>
-                <h3 className='text-lg font-semibold text-gray-900'>
+            <figure class='flex flex-col justify-center items-center p-8 text-center bg-gray-50 border-b border-gray-200 lg:border-b-0 md:p-12 lg:border-r'>
+              <blockquote class='mx-auto mb-8 max-w-2xl text-gray-500'>
+                <h3 class='text-lg font-semibold text-gray-900'>
                   Mindblowing workflow and variants
                 </h3>
-                <p className='my-4'>
+                <p class='my-4'>
                   "As someone who mainly designs in the browser, I've been a casual user of Figma,
                   but as soon as I saw and started playing with FlowBite my mind was 🤯.
                 </p>
-                <p className='my-4'>
+                <p class='my-4'>
                   Everything is so well structured and simple to use (I've learnt so much about
                   Figma by just using the toolkit).
                 </p>
-                <p className='my-4'>
+                <p class='my-4'>
                   Aesthetically, the well designed components are beautiful and will undoubtedly
                   level up your next application."
                 </p>
               </blockquote>
             </figure>
-            <figure className='flex flex-col justify-center items-center p-8 text-center bg-gray-50 border-gray-200 md:p-12'>
-              <blockquote className='mx-auto mb-8 max-w-2xl text-gray-500'>
-                <h3 className='text-lg font-semibold text-gray-900'>Efficient Collaborating</h3>
-                <p className='my-4'>
+            <figure class='flex flex-col justify-center items-center p-8 text-center bg-gray-50 border-gray-200 md:p-12'>
+              <blockquote class='mx-auto mb-8 max-w-2xl text-gray-500'>
+                <h3 class='text-lg font-semibold text-gray-900'>Efficient Collaborating</h3>
+                <p class='my-4'>
                   "This is a very complex and beautiful set of elements. Under the hood it comes
                   with the best things from 2 different worlds: Figma and Tailwind.
                 </p>
-                <p className='my-4'>
+                <p class='my-4'>
                   You have many examples that can be used to create a fast prototype for your team."
                 </p>
               </blockquote>
             </figure>
           </div>
-          <div className='text-center'>
+          <div class='text-center'>
             <a
               href='#'
-              className='py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200'>
+              class='py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200'>
               Show more...
             </a>
           </div>
         </div>
       </section>
-      <Toaster />
     </main>
   )
-}
-
-export const getServerSideProps = async ctx => {
-  const [publicKey, setPublicKey] = useState(null)
-  const [balance, setBalance] = useState(null)
-
-  const connectWallet = async () => {
-    alert("connecting wallet")
-    let key = localStorage.getItem("publicKey")
-    if (!key) {
-      let publicKeyTemp = await getPublicKey()
-      if (typeof publicKeyTemp !== "string") {
-        console.log(publicKeyTemp)
-        toast.error("Phantom Wallet not installed")
-        setTimeout(() => {
-          open("https://phantom.app/download", "_blank")
-        }, 1600)
-      }
-    }
-    let balanceTemp
-    if (!publicKeyTemp) return
-    if (!balance) {
-      balanceTemp = await getBalance(publicKeyTemp)
-    }
-    setPublicKey(publicKeyTemp)
-    setBalance(balanceTemp)
-
-    localStorage.setItem("publicKey", publicKey)
-    location.replace("/dashboard")
-  }
-  return {
-    props: {
-      connectWallet,
-      publicKey,
-      balance,
-    },
-  }
 }
--- components/DshBrdSection.jsx
+++
@@ -1,7 +0,0 @@
-import Image from "next/image"
-import Link from "next/link"
-import NavBar from "@/components/NavBar"
-
-export default function DshBrdSection({ children }) {
-  return <div className='w-full flex flex-row py-6 px-12'></div>
-}
